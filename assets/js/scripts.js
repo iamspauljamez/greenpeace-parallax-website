f0d6beca5003a2f166fcdbaf16b1b7edbf0e6367
Paul Sabandal <pjsabandal@e-lawsolutions.com>
2014-08-01 15:22:35 +0800

    - Add preloader

---
+++ assets/js/scripts.js
@@ -0,0 +1,30 @@
+/*!
+ * fastshell
+ * Fiercely quick and opinionated front-ends
+ * https://HosseinKarami.github.io/fastshell
+ * @author Hossein Karami
+ * @version 1.0.0
+ * Copyright 2014. MIT licensed.
+ */
+(function ($, window, document, undefined) {
+
+  'use strict';
+
+  $(function () {
+    var s = skrollr.init({
+        render: function(data) {
+            //Debugging - Log the current scroll position.
+            console.log(data.curTop);
+        }
+    });
+	
+  });
+
+})(jQuery, window, document);
+
+
+$(window).load(function() { // makes sure the whole site is loaded
+	$('#status').fadeOut(); // will first fade out the loading animation
+	$('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+	$('body').delay(350).css({'overflow':'visible'});
+})
